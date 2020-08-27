ecce10da9ced27e292ae3abe5887a7d25431b335
devjk1 <jkdev1@protonmail.com>
2020-08-27 21:14:54 +0000

    logic complete; small bug in checkbox value

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-<div x-data="{ isOpen: false }">
-    <button @click="isOpen = !isOpen">
-        Toggle
-    </button>
-    <h1 x-show="isOpen">
-        index.html
-    </h1>
+<div x-data="todos()">
+    <h1>My Tasks</h1>
+    <div class="todo-section">
+        <div class="incompleted-todos">
+            <div>TO-DO</div>
+            <ul>
+                <template
+                    x-for="(todo, index) in todos"
+                    :key="index"
+                >
+                    <li>
+                        <div>
+                            <div 
+                                x-text="todo.title"
+                            >
+                            </div>
+                            <input 
+                                type="checkbox"
+                                @change="completeTodo(index)"
+                            >
+                        </div>
+                    </li>
+                </template>
+            </ul>
+        </div>
+        <div class="completed-todos">
+            <div>COMPLETED</div>
+            <ul>
+                <template
+                    x-for="(todo, index) in completedTodos"
+                    :key="index"
+                >
+                    <li>
+                        <div 
+                            class="line-through"
+                            x-text="todo.title"
+                        >
+                        </div> 
+                    </li>
+                </template>
+            </ul>
+        </div>
+    </div>
+    <div class="todo-add-section">
+        <label>
+            New Task
+        </label>
+        <input 
+            type="text"
+            x-model="newTodoTitle"    
+        >
+        <button
+            @click="addTodo()"
+        >
+            Add
+        </button>
+    </div>
 </div>
+<script>
+    function todos() {
+        return {
+            todos: [],
+            completedTodos: [],
+            newTodoTitle: "",
+            newTodoId: 1,
+            addTodo() {
+                if (this.newTodoTitle.trim() === "") {
+                    return;
+                }
+
+                this.todos.push({
+                    title: this.newTodoTitle,
+                    id: this.newTodoId,
+                });
+
+                this.newTodoId++;
+                this.newTodoTitle = "";
+            },
+            completeTodo(index) {
+                this.completedTodos.push(
+                    {
+                        title: this.todos[index].title,
+                    }
+                );
+
+                this.todos.splice(index, 1);
+            },
+        };
+    }
+</script>
 @endsection
